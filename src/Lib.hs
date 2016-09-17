5569d42100c6c08d4d3aa7f668053626101c52b4
Ignat Insarov <kindaro@gmail.com>
2016-09-17 16:40:43 +0300

    Enhance: A naive brute force check.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,25 +1,34 @@
+{-# LANGUAGE
+    LambdaCase
+  #-}
+
 module Lib where
 
 
+import Control.Monad.Identity
+import Control.Monad.State
 import Control.Monad.Writer
+import Data.Array
 
+
+foldM1 f (x:xs) = foldM f x xs
 
 f = undefined
 
 f1 :: [Integer] -> Bool
 f1 (a:b:xs) = undefined
-f1 l = True
+f1 list = True
 
-whereDoesListFall :: [Integer] -> [Integer]
-whereDoesListFall xs
-    | length xs < 2 = []
-    | otherwise = snd . runWriter $ foldM f (head xs) (tail xs)
-    where
-    f :: Integer -> Integer -> Writer [Integer] Integer
-    f a b
-        | pairIsNonDecreasing a b = return b
-        | otherwise = tell [position] >> return b
-    position = 13
+-- whereDoesListFall :: [Integer] -> [Integer]
+-- whereDoesListFall xs
+--     | length xs < 2 = []
+--     | otherwise = snd . runWriter $ foldM1 f xs
+--     where
+--     f :: Integer -> Integer -> WriterT [Integer] (StateT Integer Identity) Integer
+--     f a b
+--         | pairIsNonDecreasing a b = return b
+--         | otherwise = tell [position] >> return b
+--     position = 13
 
 listIsNonDecreasing :: [Integer] -> Bool
 listIsNonDecreasing [] = True
@@ -31,4 +40,34 @@ listIsNonDecreasing (a:b:xs)
 
 pairIsNonDecreasing :: Integer -> Integer -> Bool
 pairIsNonDecreasing a b = a <= b
+
+data Answer = Answer { truth :: Bool, permutation :: Maybe [Integer] }
+    deriving Show
+
+fNaive :: [Integer] -> Answer
+fNaive list
+    | length list < 3 = Answer { truth = True, permutation = Nothing }
+    | length answers > 0 = head answers
+    | otherwise = Answer { truth = False, permutation = Nothing }
+    where
+        range = [0 .. (fromIntegral . length) list - 1]
+        answers = (filter truth) $
+            [ Answer
+                { truth = listIsNonDecreasing (swapInList x y list)
+                , permutation = if x == y then Nothing else Just [x, y]
+                }
+            | x <- range, y <- range
+            ]
+
+swapInList :: Integer -> Integer -> [a] -> [a]
+swapInList a b list
+    | a == b = list
+    | otherwise =
+        let
+            m = listArray (0, (fromIntegral . length) list - 1) list
+            a' = fromIntegral a :: Int
+            b' = fromIntegral b :: Int
+            storeSwap = m ! a'
+        in
+            elems $ m // [(a', m ! b')] // [(b', storeSwap)]
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -5,12 +5,18 @@ main = do
     putStrLn . (++) "Test cases to pass: " $ show . length $ testCases
     putStrLn . (++) "Failed cases: " $ show . length . filter not $ testCases
 
-testCases = 
-    [ pairIsNonDecreasing 1 2 == True
-    , pairIsNonDecreasing 1 1 == True
-    , pairIsNonDecreasing 1 0 == False
-    , listIsNonDecreasing [1,2,3] == True
-    , listIsNonDecreasing [1,2,2] == True
-    , listIsNonDecreasing [1,2,1] == False
-    , listIsNonDecreasing [1,2,1,2] == False
-    ]
+testCases
+    =   [ pairIsNonDecreasing 1 2 == True
+        , pairIsNonDecreasing 1 1 == True
+        , pairIsNonDecreasing 1 0 == False
+        ]
+    ++  [ listIsNonDecreasing [1,2,3] == True
+        , listIsNonDecreasing [1,2,2] == True
+        , listIsNonDecreasing [1,2,1] == False
+        , listIsNonDecreasing [1,2,1,2] == False
+        ]
+    ++  [ swapInList 1 3 [0,1,2,3,4] == [0,3,2,1,4]
+        ]
+    ++  [ truth (fNaive [1,3,2,4]) == True
+        ]
+
